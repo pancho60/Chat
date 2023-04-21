c97f9ada80e50fa48b5ca1eb83bc9b6aa4d88788
Francisco Ramirez Nuño <frco.javiersmec@.com>
2023-04-21 15:32:34 -0600

    chat

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true
+};
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBjp6pfzeeoA6nA7TGm4YA4qL6U61ISzvc',
+  authDomain: 'chatonline-6b50d.firebaseapp.com',
+  projectId: 'chatonline-6b50d',
+  storageBucket: 'chatonline-6b50d.appspot.com',
+  messagingSenderId: '870213515086',
+  appId: '1:870213515086:web:d268dc23f6b68e4ab7d01f',
+  measurementId: 'G-MX3XSZJ4V0',
+};
